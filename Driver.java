1a3e4ff92986d170ba3577c10cd886d150d20ecc
Michael Chan <mchan10@stuy.edu>
2018-10-10 14:22:09 -0400

    added distanceTo

--- Driver.java
+++ Driver.java
@@ -1,6 +1,8 @@
 public class Driver{
 	public static void main(String[] args){
-		Point p = new Point(5.5, 6.5);
+		Point p = new Point(5.0, 6.0);
+		Point o = new Point(5.0, 8.0);
 		System.out.println(p.getX());
+		System.out.println(p.distanceTo(o));
 	}	
 }
--- Point.java
+++ Point.java
@@ -14,5 +14,8 @@ public class Point{
 	public double getY(){
 		return y;
 	}	
+	public double distanceTo(Point p){
+		return Math.sqrt(Math.pow(p.x - this.x, 2) + Math.pow(p.y - this.y, 2));
+	}	
 }	
 	
